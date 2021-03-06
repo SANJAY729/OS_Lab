646817aca91c16e6112566ed48e94c828e0b41a5
D.VAMSHIDHAR REDDY <vamshi.dudyala@gmail.com>
2021-03-06 14:22:11 +0530

    os too ez

--- a5/a5_p2.cpp
+++ a5/a5_p2.cpp
@@ -7,16 +7,18 @@
 #include <sys/ipc.h> 
 #include <sys/shm.h> 
 #include <sys/time.h>
+#include <sys/syscall.h>
 #include <chrono>
 #include <bits/stdc++.h>
 using namespace std;
 
 #define MAX_SIZE_PRIORITY_QUEUE 100
+#define gettid() syscall(SYS_gettid)
 
 int max_queue_size = 8;
 
 typedef struct {
-    int process_id;
+    int thread_id;
     int producer_number;
     int priority;
     int compute_time;
@@ -31,15 +33,21 @@ typedef struct {
     pthread_mutex_t lock;
 } data;
 
-data* initialize(int shmid) {
-    data* jobs_info = (data*)shmat(shmid,(void*)0,0);
-    jobs_info->size = 0;
-    jobs_info->job_created = 0;
-    jobs_info->job_completed = 0;
+typedef struct {
+    data* jobs_info;
+    int thread_number;
+    int total_jobs;
+} param;
+
+data initialize() {
+    data jobs_info;
+    jobs_info.size = 0;
+    jobs_info.job_created = 0;
+    jobs_info.job_completed = 0;
     pthread_mutexattr_t lock_attr;
 	pthread_mutexattr_init(&lock_attr);
 	pthread_mutexattr_setpshared(&lock_attr, PTHREAD_PROCESS_SHARED);
-	pthread_mutex_init(&jobs_info->lock, &lock_attr);
+	pthread_mutex_init(&jobs_info.lock, &lock_attr);
     return jobs_info;
 }
 
@@ -107,19 +115,19 @@ job delete_job(data* jobs_info) {
     return temp;
 }
 
-job make_job(int producer_number, int process_id) {
+job make_job(int producer_number, int thread_id) {
     job x;
     x.compute_time = rand()%4 + 1;
     x.job_id = rand()%100000 + 1;
     x.priority = rand()%10 + 1;
     x.producer_number = producer_number;
-    x.process_id = process_id;
+    x.thread_id = thread_id;
     return x; 
 }
 
 void print(job x) {
     cout<<"JOB ID "<<x.job_id<<endl;
-    cout<<"PRODUCER PROCESS ID "<<x.process_id<<endl;
+    cout<<"PRODUCER THREAD ID "<<x.thread_id<<endl;
     cout<<"PRIORITY "<<x.priority<<endl;
     cout<<"PRODUCER NUMBER "<<x.producer_number<<endl;
     cout<<"COMPUTE TIME "<<x.compute_time<<endl;
@@ -130,7 +138,13 @@ void lag(int time) {
     while(clock() < start + time * CLOCKS_PER_SEC);
 }
 
-void produce_job(data* jobs_info, int producer_number, int process_id, int total_jobs) {
+void* produce_job(void* arg){
+    //extracting the parameters from void* arg
+    param* function_param = (param*)arg;
+    data* jobs_info = function_param->jobs_info;
+    int producer_number = function_param->thread_number;
+    int thread_id = gettid();
+    int total_jobs = function_param->total_jobs;
     while(1) {
         lag(rand()%4);
         pthread_mutex_lock(&jobs_info->lock);
@@ -139,7 +153,7 @@ void produce_job(data* jobs_info, int producer_number, int process_id, int total
             break;
         }
         if (jobs_info->size < max_queue_size) {
-            job x = make_job(producer_number,process_id);
+            job x = make_job(producer_number,thread_id);
             cout<<"***JOB PRODUCED***"<<endl;
             print(x);
             jobs_info->job_created++;
@@ -147,10 +161,15 @@ void produce_job(data* jobs_info, int producer_number, int process_id, int total
         }
         pthread_mutex_unlock(&jobs_info->lock);
     }
-    return;
 }
 
-void consume_job(data* jobs_info, int consumer_number, int process_id, int total_jobs) {
+void* consume_job(void* arg){
+    //extracting the parameters from void* arg
+    param* function_param = (param*)arg;
+    data* jobs_info = function_param->jobs_info;
+    int consumer_number = function_param->thread_number;
+    int thread_id = gettid();
+    int total_jobs = function_param->total_jobs;
     while(1) {
         lag(rand()%4);
         pthread_mutex_lock(&jobs_info->lock);
@@ -162,15 +181,17 @@ void consume_job(data* jobs_info, int consumer_number, int process_id, int total
             job x = delete_job(jobs_info);
             cout<<"***JOB CONSUMED***"<<endl;
             cout<<"CONSUMER NUMBER "<<consumer_number<<endl;
-            cout<<"CONSUMER PROCESS ID "<<process_id<<endl;
+            cout<<"CONSUMER THREAD ID "<<thread_id<<endl;
             print(x);
             lag(x.compute_time);
             jobs_info->job_completed++;
         }
         pthread_mutex_unlock(&jobs_info->lock);
     }
-    return;
 }
+
+//Global Variables which are used as shared memory
+data info = initialize();
 
 int main() {
     srand(time(0));
@@ -179,51 +200,35 @@ int main() {
     cin>>NC;
     cin>>total_jobs;
 
-    key_t key = 777;
-    int shmid = shmget(key, sizeof(data), 0660|IPC_CREAT);
-    if (shmid < 0) {
-        cout<<"lol1"<<endl;
-        exit(0);
-    }
-    data* jobs_info = initialize(shmid);
     auto begin = std::chrono::high_resolution_clock::now();
-    pid_t pid;
-    for (int i = 1; i<=NP; i++) {
-        pid = fork();
-        if (pid < 0) {
-            cout<<"lol2"<<endl;
-            exit(0);
-        }
-        else if (pid == 0) { //inside child process
-            srand(time(0) ^ i*7);
-            produce_job(jobs_info,i,getpid(),total_jobs);
-            exit(0);
-        }
+
+    pthread_t tids[NP + NC + 1]; //using as indexed from 1
+    param args_threads[NP + NC + 1]; //using as indexed from 1
+
+    for(int i = 1; i <= NP; i++) {
+        args_threads[i].jobs_info = &info;
+        args_threads[i].thread_number = i;
+        args_threads[i].total_jobs = total_jobs;
+        
+        pthread_attr_t attr;
+        pthread_attr_init(&attr);
+        pthread_create(&tids[i],&attr, produce_job, &args_threads[i]);
     }
-    for (int i = 1; i<=NC; i++) {
-        pid = fork();
-        if (pid < 0) {
-            cout<<"lol3"<<endl;
-            exit(0);
-        }
-        else if (pid == 0) { //inside child process
-            srand(time(0) ^ i);
-            consume_job(jobs_info,i,getpid(),total_jobs);
-            exit(0);
-        }
+    for(int i = NP + 1; i <= NP + NC; i++) {
+        args_threads[i].jobs_info = &info;
+        args_threads[i].thread_number = i - NP;
+        args_threads[i].total_jobs = total_jobs;
+        
+        pthread_attr_t attr;
+        pthread_attr_init(&attr);
+        pthread_create(&tids[i],&attr, consume_job, &args_threads[i]);
     }
-    while(1) {
-        lag(2);
-        pthread_mutex_lock(&jobs_info->lock);
-        if (jobs_info->job_created == jobs_info->job_completed && jobs_info->job_created == total_jobs) {
-            auto end = std::chrono::high_resolution_clock::now();
-            auto time_spent = std::chrono::duration_cast<std::chrono::microseconds>(end-begin);
-            cout<<"Time spent in seconds is "<<time_spent.count()/1000000<<endl;
-            pthread_mutex_unlock(&jobs_info->lock);
-            break;
-        }
-        pthread_mutex_unlock(&jobs_info->lock);
-    }
-    cout<<"i am done"<<endl;
+
+    for (int i = 1; i <= NP+NC; i++)
+        pthread_join(tids[i], NULL);
+
+    auto end = std::chrono::high_resolution_clock::now();
+    auto time_spent = std::chrono::duration_cast<std::chrono::microseconds>(end-begin);
+    cout<<"Time spent in seconds is "<<time_spent.count()/1000000<<endl;
     return 0;
 }
